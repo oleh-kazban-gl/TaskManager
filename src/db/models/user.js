adce0d1263107b0d93f04bc9b9e3267a15d56626
Oleh Kazban <kazban.oleg@gmail.com>
2024-12-04 18:25:58 +0200

    Add User model

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -1,11 +1,41 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const User = mongoose.model('User', {
     name: {
         type: String,
+        required: true,
+        trim: true,
     },
     age: {
         type: Number,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid');
+            }
+        },
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6,
+        trim: true,
+        validate(value) {
+            const prohibitedValues = ['password'];
+
+            if (
+                prohibitedValues.some(
+                    (pV) => pV.toLowerCase() === value.toLowerCase()
+                )
+            ) {
+                throw new Error(`Password can't include prohibited values`);
+            }
+        },
     },
 });
 
